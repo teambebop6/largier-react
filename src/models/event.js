c496248b9b4461084a2f13a3cc1b5cc6cc0060bd
Florian Rudin <flaudre@gmail.com>
2018-02-19 21:55:09 +0800

    Added event model

---
+++ src/models/event.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(mongoose.connection);
+
+const Event = new mongoose.Schema({
+  _id: Number,
+  title: String,
+  location: String,
+  venue: String,
+  date: {
+    type: Date,
+    default: Date.Now
+  },
+  link: String,
+  type: String,
+  order: {
+    type: Number,
+    default: -1
+  },
+  visible: {
+    type: Boolean,
+    default: false
+  },
+  creation_date: {
+    type: Date,
+    default: Date.now
+  },
+  last_modified_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+Event.plugin(autoIncrement.plugin, 'Event');
+
+export default mongoose.model('Event', Event);
