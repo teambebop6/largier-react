a381e2060fbcf1efdf5535d52c2d3484f8cea09d
Florian Rudin <flaudre@gmail.com>
2018-02-19 21:56:10 +0800

    Added concerts route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,34 @@ import express from 'express';
 import auth from './auth';
 
 // Load subroutes
-import admin_crud_router from './admin/crudTemplate'
+import admin_events_router from './admin/events'
+
+import Event from '../models/event';
 
 const router = express.Router();
 
 // Initialize subroutes
-router.use('/admin/crudTemplate', admin_crud_router)
+router.use('/admin/events', admin_events_router)
 
-router.get('/', (req, res) => {
-  res.json({
-    result: 'Welcome to TB6.'
+// Get concerts
+router.get('/concerts', function (req, res, next) {
+  Event.find().sort({'date': 'asc'}).exec((err, events) => {
+    if(err){ return res.json(err); }
+
+    const upcoming_concerts = events.filter((e) => {
+        return (e.type == "concert" && (Date.parse(e.date) >= Date.now()))
+      });
+
+    const past_concerts = events.filter((e) => {
+        return (e.type == "concert" && (Date.parse(e.date) < Date.now()))
+      }).reverse();
+    
+    res.json({
+      data: {
+        upcoming_concerts: upcoming_concerts,
+        past_concerts: past_concerts,
+      }
+    });
   });
 });
 
