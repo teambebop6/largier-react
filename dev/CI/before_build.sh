457d5469a743d5c4cd8b0a8d2b3ac8c34f31254c
henryhuang <h1886@outlook.com>
2022-05-18 21:56:28 +0800

    action file change

--- dev/CI/before_build.sh
+++ dev/CI/before_build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-echo "$ECOSYSTEM_CONFIG_JS" > ecosystem.config.js
-cp ecosystem.config.js dist/
-
 [ ! -d "src/largier-secret" ] && mkdir -p src/largier-secret
 echo "$CONFIG_DEV" > src/largier-secret/development.js
 echo "$CONFIG_PROD" > src/largier-secret/production.js
